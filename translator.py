dfcf2b2e7028c5fab371dbd7f1ef2e32f441f26c
zooo <imzooo@163.com>
2016-07-07 08:02:25 +0800

    添加保存查询单词功能

--- translator.py
+++ translator.py
@@ -3,7 +3,8 @@ import urllib.parse
 import json
 url = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule&smartresult=ugc&sessionFrom=fanyi.logo'
 #添加header
-myComputer = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'}
+userAgent = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'}
+wordList = open('./ComputerScience.txt','a')
 while True:
     content = input("请输入要翻译的内容：")
     if content in ['q','Q']:
@@ -20,7 +21,7 @@ while True:
     }
     data = urllib.parse.urlencode(data)#得到的是一个字符串，需要将其转换成二进制形式
     data = urllib.parse.unquote_to_bytes(data)
-    require = urllib.request.Request(url,data,myComputer)
+    require = urllib.request.Request(url,data,userAgent)
     response = urllib.request.urlopen(require)
     #response = urllib.request.urlopen('http://fanyi.youdao.com/?keyfrom=fanyi.logo')#需要完整的网页地址
     html = response.read()
@@ -30,3 +31,5 @@ while True:
     html = json.loads(html)#'translateResult': [[{'tgt': '好好学习', 'src': 'good good study'}]]
     result = html['translateResult'][0][0]['tgt']#dict->list->list->dict->result
     print('翻译结果(来自有道)：%s'%result)
+    wordList.write(content+'  '+result+'\n')
+wordList.close()
